c2a21febef3b44ec71328d5593eef6ea2dccf1ca
丶曲終人散ゞ <1254350772@qq.com>
2026-03-29 10:43:53 +0800

    Disable UDP relay - TCP video working, UDP forwarding issue needs further investigation

--- Scrcpy/server/src/main/java/qzrs/Scrcpy/server/Server.java
+++ Scrcpy/server/src/main/java/qzrs/Scrcpy/server/Server.java
@@ -160,18 +160,10 @@ public final class Server {
       serverSocket.close();
     }
 
-    // 尝试连接UDP中继
-    String deviceId = "relay-" + Options.serverPort;
-    udpRelaySender = new UdpRelaySender();
-    Log.e(TAG, "[Server] 尝试连接UDP中继, deviceId=" + deviceId);
-    if (udpRelaySender.connect(deviceId)) {
-      useUdpRelay = true;
-      Log.e(TAG, "[UDP] UDP中继连接成功，启用UDP视频传输");
-    } else {
-      Log.e(TAG, "[UDP] UDP中继连接失败，使用TCP传输");
-      useUdpRelay = false;
-      udpRelaySender = null;
-    }
+    // 暂时禁用UDP - TCP视频已正常工作
+    useUdpRelay = false;
+    udpRelaySender = null;
+    Log.e(TAG, "[UDP] UDP中继已禁用，使用TCP传输视频");
   }
 
   private static void executeVideoOut() {
